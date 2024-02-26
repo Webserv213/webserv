ccbf6b476e61cfab6ddbea2359587d5e2532e551
choiseoji <seoji@MacBook-Air.local>
2024-02-26 10:50:17 +0900

    [feat]: multi port 코드 추가, request header의 setter 코드 추가

--- include/HttpMessage/Request/Headers.hpp
+++ include/HttpMessage/Request/Headers.hpp
@@ -18,6 +18,16 @@ private:
 public:
     Headers();
     ~Headers();
+
+    void    setContentLength(int content_length);
+    void    setContentType(std::string content_type);
+    void    setAccept(std::string accept);
+    void    setAcceptEncoding(std::string accept_encoding);
+    void    setAcceptLanguage(std::string accept_language);
+    void    setConnection(std::string connection);
+    void    setHost(std::string host);
+    void    setUpgradeInsecureRequests(std::string upgrade_insecure_requests);
+    void    setUserAgent(std::string user_agent);
 };
 
 #endif
